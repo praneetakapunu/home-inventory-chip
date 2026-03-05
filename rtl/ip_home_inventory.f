f6a37db98a9451e3fbb4b72ecee274c7021d29fc
Madhuri (OpenClaw) <praneet.chip@gmail.com>
2026-03-05 08:31:12 +0000

    rtl/filelist: include adc_soc_frame_unpack in canonical IP filelist

--- rtl/ip_home_inventory.f
+++ rtl/ip_home_inventory.f
@@ -14,6 +14,7 @@ rtl/home_inventory_event_detector.v
 rtl/adc/adc_drdy_sync.v
 rtl/adc/adc_stream_fifo.v
 rtl/adc/adc_spi_frame_capture.v
+rtl/adc/adc_soc_frame_unpack.v
 rtl/adc/adc_frame_to_fifo.v
 rtl/adc/adc_streaming_ingest.v
 rtl/home_inventory_top.v
